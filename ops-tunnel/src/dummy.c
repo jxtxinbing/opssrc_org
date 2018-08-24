834631eebaf10a6ddc7787bc74b1f594006de107
jxtxinbing <450275009@qq.com>
2018-08-24 18:43:29 +0800

    commit source code

---
+++ ops-tunnel/src/dummy.c
@@ -0,0 +1,12 @@
+/* NOTE: This file is just to avoid the linkage issues for CMake file,
+ *       Remove it after adding the ops-tunnel code and change the CMake
+ *       file accordingly.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("OPS-TUNNEL!\n");
+    return 0;
+}
